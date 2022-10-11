e0b9971b9221429ac7e0a7873434a77945040b0e
Firat Sakar <frtskr0@gmail.com>
2022-10-11 23:45:40 +0300

    padding values

---
+++ lib/padding_values.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class PaddingValues {
+  final EdgeInsets PD_T_10 = const EdgeInsets.only(top: 10);
+  final EdgeInsets PD_B_10 = const EdgeInsets.only(bottom: 10);
+  final EdgeInsets PD_L_10 = const EdgeInsets.only(left: 10);
+  final EdgeInsets PD_R_10 = const EdgeInsets.only(right: 10);
+  final EdgeInsets PD_TLR_10 = const EdgeInsets.only(top: 10, right: 10, left: 10);
+}
