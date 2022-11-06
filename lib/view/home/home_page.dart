1b3a0031af23de7dd638389b5c73a18237b7affa
Firat Sakar <frtskr0@gmail.com>
2022-11-06 17:44:22 +0300

    main page bottom nav

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:todo_list/padding_values.dart';
+import 'package:todo_list/project_colors.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,40 +9,74 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+class _HomePageState extends State<HomePage> with CustomColors, TickerProviderStateMixin, PaddingValues {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: _BottomNavItems.values.length, vsync: this);
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: const Color.fromRGBO(33, 173, 137, 1.0),
-        foregroundColor: Colors.white,
-        child: const Icon(Icons.add_outlined),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.grey.shade800,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: "Home",
+    return DefaultTabController(
+      length: _BottomNavItems.values.length,
+      child: Scaffold(
+        extendBody: true,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: CustomColors().lightSeaGreen,
+          foregroundColor: CustomColors().white,
+          child: const Icon(Icons.add_outlined),
+          onPressed: () {},
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          notchMargin: HomePageDoubleValues.notchValue,
+          shape: const CircularNotchedRectangle(),
+          child: TabBar(
+            indicatorColor: CustomColors().white,
+            indicatorSize: TabBarIndicatorSize.label,
+            labelColor: CustomColors().lightSeaGreen,
+            unselectedLabelColor: CustomColors().dimGray,
+            labelStyle: Theme.of(context).textTheme.subtitle1?.copyWith(
+                  fontSize: HomePageDoubleValues.selectedFontSize,
+                ),
+            unselectedLabelStyle: Theme.of(context).textTheme.subtitle1?.copyWith(
+                  fontSize: HomePageDoubleValues.unselectedFontSize,
+                ),
+            controller: _tabController,
+            tabs: [
+              Tab(
+                iconMargin: PaddingValues().PD_B_5,
+                text: _BottomNavItems.Home.name,
+                icon: const Icon(Icons.home_outlined),
+              ),
+              Tab(
+                iconMargin: PaddingValues().PD_B_5,
+                text: _BottomNavItems.Profile.name,
+                icon: const Icon(Icons.person_outline),
+              )
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            label: "Profile",
+        ),
+        body: TabBarView(controller: _tabController, children: [
+          Container(
+            color: Colors.red,
           ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color.fromRGBO(33, 173, 137, 1.0),
-        onTap: _onItemTapped,
+          Container(
+            color: Colors.white,
+          ),
+        ]),
       ),
     );
   }
+}
+
+enum _BottomNavItems { Home, Profile }
+
+class HomePageDoubleValues {
+  static double selectedFontSize = 15.0;
+  static double unselectedFontSize = 15.0;
+  static double notchValue = 10.0;
 }
