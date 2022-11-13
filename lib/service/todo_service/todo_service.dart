ac7f3c02e6a1064095b73a86ef040e3b8f8780e3
FIRAT ŞAKAR <frtskr0@gmail.com>
2022-11-14 00:13:57 +0300

    override getUserIdTodos method

--- lib/service/todo_service/todo_service.dart
+++ lib/service/todo_service/todo_service.dart
@@ -15,7 +15,21 @@ class TodoService extends ITodoService {
       final response = await dio.get(services.todos.name);
       if (response.statusCode == HttpStatus.ok) {
         final _todos = response.data;
+        if (_todos is List) {
+          return _todos.map((e) => TodoModel.fromJson(e)).toList();
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
+  @override
+  Future<List<TodoModel>?> getUserIdTodos(int id) async {
+    try {
+      final response = await dio.get("${services.todos.name}/?userId=$id");
+      if (response.statusCode == HttpStatus.ok) {
+        final _todos = response.data;
         if (_todos is List) {
           return _todos.map((e) => TodoModel.fromJson(e)).toList();
         }
