502d578c9168f3326c2417b7dd61fe7fbfa94f5f
FIRAT ŞAKAR <frtskr0@gmail.com>
2022-11-16 22:57:47 +0300

    listed todos

--- lib/view/tabBarViews/todo_page.dart
+++ lib/view/tabBarViews/todo_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list/model/todo_model.dart';
 import 'package:todo_list/model/user_model.dart';
+import 'package:todo_list/progress_indicator.dart';
 import 'package:todo_list/project_colors.dart';
 import 'package:todo_list/service/todo_service/ITodo_service.dart';
 import 'package:todo_list/service/todo_service/todo_service.dart';
@@ -12,9 +13,12 @@ class ToDoPage extends StatefulWidget {
   State<ToDoPage> createState() => _ToDoPageState();
 }
 
-class _ToDoPageState extends State<ToDoPage> {
+class _ToDoPageState extends State<ToDoPage> with ProgressIndicatorView, CustomColors {
   ITodoService? _todoService;
+
   List<TodoModel>? _todoItems;
+
+  bool isLoading = false;
 
   _ToDoPageState() {}
 
@@ -27,21 +31,33 @@ class _ToDoPageState extends State<ToDoPage> {
 
   void _getTodos() async {
     _todoItems = await _todoService?.getUserIdTodos(widget.user?.id ?? 0);
+    changedLoading();
+  }
+
+  void changedLoading() {
+    setState(() {
+      isLoading = !isLoading;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("To Do Page")),
-      body: Container(
-        child: Text(
-          _todoItems?[0].title.toString() ?? '',
-          style: Theme.of(context).textTheme.subtitle1?.copyWith(
-                fontSize: 18,
-                color: CustomColors().white,
-              ),
-        ),
-      ),
+      body: isLoading
+          ? ListView.builder(
+              itemCount: _todoItems?.length ?? 0,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                      title: Text(
+                    _todoItems?[index].title ?? '',
+                    style: Theme.of(context).textTheme.headline6?.copyWith(
+                          color: _todoItems?[index].completed ?? false ? lightSeaGreen : red,
+                        ),
+                  )),
+                );
+              })
+          : Center(child: circularProgressIndicator()),
     );
   }
 }
